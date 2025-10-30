c4888ba354e7da9a7440a21f3e89b5c3ec081ba8
azamat-def <azzamat.yusupov@gmail.com>
2025-10-30 21:53:23 +0500

    Update YusupovAzamat.py
    this commit was made in github

--- venv/YusupovAzamat.py
+++ venv/YusupovAzamat.py
@@ -1,5 +1,7 @@
 def main():
     print('My first git programs')
     print('And I change code every day')
+    print('The thirt line')
+    print('unkonw writes here')
     if __name__ == '__main__':
-    main()
+        main()
